89c327c1f4081871ea644fd1f5227ee1ec55eb10
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-06-29 13:22:49 -0500

    basic connection to Mongo working

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const userRoutes = require('./user-routes')
+
+// add prefix of '/pizzas' to routes created in pizza-routes.js
+router.use('/users', userRoutes)
+
+
+module.exports = router
