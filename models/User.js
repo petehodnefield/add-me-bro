a52da0f07dec1a0b820ae80caa3f912fc6047a2e
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-06-30 08:56:00 -0500

    models created

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const {Schema, model, Types} = require('mongoose')
+// TODO: add dataformat
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    }, 
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+    
+)
+
+const User = model('User', UserSchema)
+
+module.exports = User
